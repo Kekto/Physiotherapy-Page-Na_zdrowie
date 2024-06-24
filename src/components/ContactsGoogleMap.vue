0f453f9c2441e801be3abf907706bc2240361bc9
Kekto <lekto8814@gmail.com>
2024-06-24 17:28:25 +0200

    Google Maps Api

---
+++ src/components/ContactsGoogleMap.vue
@@ -0,0 +1,17 @@
+<template>
+  <GoogleMap
+  api-key="AIzaSyD4vD_jiEOYTc0sq7_3gJ0xWG1YqSn2aJc"
+  style="width: 50vw; height: 500px"
+  :center="center"
+  :zoom="15"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
+
+
+<script setup lang="ts">
+import { GoogleMap, Marker } from 'vue3-google-map'
+
+const center = { lat: 51.241562099387075, lng: 22.518481991812706 }
+</script>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="home">
+  <div class="contact">
     <TitleBanner title="Kontakt"/>
+    <div>
+      <ContactsGoogleMap/>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import TitleBanner from '../components/TitleBanner.vue'
-
+import ContactsGoogleMap from '../components/ContactsGoogleMap.vue';
 </script>
 
 <style lang="scss" scoped>
