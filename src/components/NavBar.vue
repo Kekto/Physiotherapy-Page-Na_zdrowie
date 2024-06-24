97aa8af996be1d175dfed84238f9d3c45c4740bb
Kekto <lekto8814@gmail.com>
2024-06-24 12:11:41 +0200

    init

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="nav">
+        <div class="nav__item" v-for="link in LINKS" :key="link.value">
+            <router-link :to="link.route">{{link.value}}</router-link>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const LINKS = [
+    {route: "/", value: "Strona główna"},
+    {route: "/about", value: "O mnie"},
+    {route: "/", value: "Oferta"},
+    {route: "/", value: "Jak pracuję"},
+    {route: "/", value: "Zabiegi"},
+    {route: "/", value: "Cennik"},
+    {route: "/", value: "Kontakt"}
+]
+</script>
+
+<style lang="scss" scoped>
+.nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: right;
+    gap: 12px;
+}
+</style>
